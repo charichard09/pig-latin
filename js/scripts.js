05966291e6a84451a57de69fbcfd28d12f6fcc81
Richard <charichard09@gmail.com>
2022-10-26 19:44:53 -0700

    add to pigLatin() to pass TDD test 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 // Business Logic
-const textPassageGlobal = "quack";
+const textPassageGlobal = "The duck goes quack";
 
-function pigLatin(word) {
-  const wordArray = word.split("");
+// Test: "It will test multiple words where 1 begins with 'qu', if true move 'qu' to back and add 'ay'."
+// Code:
+// const textPassage = "The duck goes quack";
+// pigLatin(textPassage);
+// Expected Output: "The duck goes ackquay"
+function pigLatin(textPassage) {
+  const textPassageArray = textPassage.split(" ");
+  let pigLatin = [];
 
-  wordArray.push(wordArray.shift());
-  wordArray.push(wordArray.shift()); 
-  
-  output = wordArray.join("") + "ay";
+  textPassageArray.forEach(function(word) {
+    const wordArray = word.split("");
 
-  return output //should be: "ackquay"
+    if (wordArray[0] === "q" && wordArray[1] === "u") {
+
+      wordArray.push(wordArray.shift());
+      wordArray.push(wordArray.shift()); 
+    
+      pigLatin.push(wordArray.join("") + "ay");
+    } else {
+      pigLatin.push(word);
+    }
+  });
+
+  return pigLatin.join(" "); //should be: "The duck goes ackquay"
 }
 
 
